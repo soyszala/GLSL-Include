992121a8912e8c6401bd9a1655a25ad7e18c3b54
Szala <65504923+soyszala@users.noreply.github.com>
2023-04-21 09:27:19 +0200

    Upload project

--- README.md
+++ README.md
@@ -1,1 +1,216 @@
 # GLSL-Include
+
+**OpenGL** doesn't have a standard way to manage include/import statements in **glsl shader files**. But if you are reading this, you probably have some utility functions, that you'd like to use in multiple shader files and as a programmer, code repetition - hopefully - bothers you. This small library is here to address this inconvenience. It provides simple C-like **#include** capability for GLSL shader files (or any text files really).
+
+## Why does this exist?
+
+Well, a simple include implementation with file loading and text replacement is something, that probably anyone could do... but no one really wants to deal with it, error handling and paths can get a bit messy, if you want to be safe. To me at least this was a "nice to have" feature, that I didn't touch for a long time.
+
+But one day I thought I'd do it, and while I was searching through the internet I didn't find a solution, that I felt convinient enough, so I though I'd implement this and might as well share it. So if you don't want to mess with this on your own, you might save an hour or two, that's all.
+
+## Features
+
+ This library is capable of:
+- **Loading shader files**
+- Finding **custom include keywords** and replace them by
+- **Inserting the contents of** the corresponding **file**
+- **Include** more **files recursively**
+- Handling relative paths in different environments
+- Addressing the issues of accidental **self includes**, **duplicate includes** and **circular dependencies**
+- Warning you about **missing files**
+
+## Example usage
+
+Simply include the **single header** file in your project, create a ShaderLoader instance, choose your keyword and load any number of files by calling the provided function:
+
+```cpp
+#include "glsl_include.hpp"
+```
+```cpp
+glsl_include::ShaderLoader sl = glsl_include::ShaderLoader("//#include");
+std::string shader_source_code1 = sl.load_shader("/path/to/your_shader.glsl");
+std::string shader_source_code2 = sl.load_shader("/path/to/your_next_shader.glsl");
+```
+
+Rules of your include statement:
+- Your custom keyword must be at the start of the line
+- Your custom keyword must be followed by a path enclosed in double quotes
+- Given path will be relative to the original files path
+
+Side notes:
+- I settled on using `//#include` as my custom keyword, because it resembles C, but writing it as a comment made some syntax highlighters stop complaining about it.
+- For any hardcore Windows users out there: You can use single backslash in the shader include statements, it should not cause trouble.
+
+### Example source code and output
+
+*/path/to/your_shader.glsl*
+```glsl
+#version 330 core
+layout (location = 0) in vec3 aPos;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
+// Including myself by accident
+//#include "your_shader.glsl"
+// Including myself by accident
+
+//#include "common.glsl"
+
+// Duplicate include avoided
+//#include "your_dir/functions.glsl"
+// Duplicate include avoided
+
+// Missing include
+//#include "missing_file.glsl"
+// Missing include
+
+void main()
+{
+	gl_Position = projection * view * model * vec4(aPos, 1.0);
+}
+```
+
+*/path/to/common_structs.glsl*
+```glsl
+//###################
+//### common.glsl ###
+//###################
+
+//#include "your_dir\functions.glsl"
+
+// Circular dependency avoided
+//#include "your_shader.glsl"
+// Circular dependency avoided
+
+struct Material {
+    sampler2D diffuse;
+    sampler2D specular;
+    float     shininess;
+};  
+
+struct DirLight {
+	vec3 direction;
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+};
+//######################
+```
+
+*/path/to/your_dir/functions.glsl*
+```glsl
+//######################
+//### functions.glsl ###
+//######################
+
+vec3 my_funcion1(in vec3) {
+    //...
+}
+
+vec3 my_funcion2(in vec3) {
+    //...
+}
+
+vec3 my_funcion3(in vec3) {
+    //...
+}
+//######################
+```
+
+*String returned from load_shader()*
+```glsl
+#version 330 core
+layout (location = 0) in vec3 aPos;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
+// Including myself by accident
+// Including myself by accident
+
+//###################
+//### common.glsl ###
+//###################
+
+//######################
+//### functions.glsl ###
+//######################
+
+vec3 my_funcion1(in vec3) {
+    //...
+}
+
+vec3 my_funcion2(in vec3) {
+    //...
+}
+
+vec3 my_funcion3(in vec3) {
+    //...
+}
+//######################
+
+
+// Circular dependency avoided
+// Circular dependency avoided
+
+struct Material {
+    sampler2D diffuse;
+    sampler2D specular;
+    float     shininess;
+};  
+
+struct DirLight {
+        vec3 direction;
+        vec3 ambient;
+        vec3 diffuse;
+        vec3 specular;
+};
+//######################
+
+
+// Duplicate include avoided
+// Duplicate include avoided
+
+// Missing include
+
+// Missing include
+
+void main()
+{
+        gl_Position = projection * view * model * vec4(aPos, 1.0);
+}
+```
+
+*Terminal output*
+```
+WARNING [ ShaderLoader::load_shader ]: '/path/to/your_shader.glsl' tried to include itself
+ERROR [ ShaderLoader::load_shader ]: Failed to start fstream, check if '/path/to/missing_file.glsl' exists
+```
+
+## Build & Requirements
+No special build process required, just include *glsl_include.hpp* in your project.
+
+No dependencies, the only requirement is **C++11 standard**.
+
+Tested on:
+```
+Ubuntu 18.04
+GCC 7.5.0, GCC 8.4.0
+
+Windows 11
+Microsoft Visual C++ 2022
+```
+If anyone feels like verifying builds on other OS-es or by using different compilers, I can include those here as well.
+
+
+## Development
+For now, I consider this library mostly finished, so my only plans are:
+- Maintenance, bug fixes
+- Test more OS-s, compilers
+
+## Contribution
+
+If you find any errors or have an idea to improve this small library, please open an **Issue**. Help is appreciated.
---
+++ glsl_include.hpp
@@ -0,0 +1,196 @@
+/*
+LICENCE
+
+MIT License
+
+Copyright (c) 2023 Szalai Balázs
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#ifndef _GLSL_INCLUDE_H
+#define _GLSL_INCLUDE_H
+
+//################
+//### Includes ###
+//################
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <regex>
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+//####################
+//### Declarations ###
+//####################
+namespace glsl_include {
+
+    /**
+     *  Init once, load any GLSL shader files by calling load_shader()
+     */
+    class ShaderLoader {
+    public:
+
+        ShaderLoader(const char* _custom_keyword)
+            : include_keyword(_custom_keyword)
+        {
+            already_included.reserve(10);
+        }
+
+
+        /**
+        *   Loads a shader file and handles #include statements.
+        * 
+        *   @param file_path The path to the shader file to be loaded.
+        *   @param include_kw The custom keyword to be used in the shader file to indicate an #include statement.
+        * 
+        *   @return A string containing the contents of the shader file, with any custom #include statements replaced by their contents.
+        */
+        std::string load_shader(const std::string& file_path)
+        {
+            // If size is 0, it indicates, that we are at the top of the recursive load stack
+            bool stack_top = (already_included.size() == 0);
+            if (stack_top) {
+                already_included.emplace_back(file_path);
+            }
+
+            std::string ret_data = "";
+            std::ifstream file(file_path);
+            if (!file.good())
+            {
+                std::cout << "ERROR [ ShaderLoader::load_shader ]: Failed to start fstream, check if '" << file_path << "' exists\n";
+                return ret_data;
+            }
+            if (file.is_open())
+            {
+                std::string line;
+                while (getline(file, line))
+                {
+                    // Custom include statement has to be at the start of the line
+                    if (line.find(include_keyword) == 0)
+                    {
+                        // Get path between double quotes
+                        std::string rel_include_path = extract_first_between(line.substr(include_keyword.length()), '"', '"');
+                        // Modify path according to os
+                        #ifdef _WIN32
+                            std::replace(rel_include_path.begin(), rel_include_path.end(), '/', '\\');
+                        #elif __linux__
+                            std::replace(rel_include_path.begin(), rel_include_path.end(), '\\', '/');
+                        #endif
+                        std::string full_include_path = extract_path(file_path) + rel_include_path;
+
+                        // Avoid including self
+                        if (file_path == full_include_path) {            
+                            std::cout << "WARNING [ ShaderLoader::load_shader ]: '"<< file_path <<"' tried to include itself\n";
+                            continue;
+                        }
+                        else {
+                            bool include_flag = true;
+                            // Check if current file already included
+                            for (const auto& file_to_check : already_included)
+                            {
+                                // Avoid duplicate includes
+                                if (file_to_check == full_include_path) {
+                                    include_flag = false;
+                                    break;
+                                }
+                            }
+                            // If not yet included, push path to include vector and replace line with file contest
+                            if (include_flag)
+                            {
+                                already_included.push_back(full_include_path);
+                                // Repeat recurively
+                                ret_data += load_shader(full_include_path) + "\n";
+                            }
+                        }
+                    }
+                    else {
+                        ret_data += line + "\n";
+                    }
+                }
+                file.close();
+            }
+            else
+            {
+                std::cout << "ERROR [ ShaderLoader::load_shader ]: Unable to open file '" << file_path << "'\n";
+            }
+            // We are back to the first call
+            if (stack_top) {
+                already_included.clear();
+            }
+            return ret_data;
+        }
+
+    private:
+        // Store already included files
+        std::vector<std::string> already_included;
+        // Custom keyword
+        std::string include_keyword;
+
+        /**
+        *   Helper function that strips filename from a path, basically getting the path to the directory containing the file
+        * 
+        *   @param file_path Input path
+        * 
+        *   @return Path without the filename, or an empty string if no separator character fround
+        */
+        std::string extract_path(const std::string& path)
+        {
+            // Find the position of the last directory separator
+            std::size_t pos = path.find_last_of("\\/");
+            
+            // Strip fname from path
+            if (pos != std::string::npos) {
+                return path.substr(0, pos+1);
+            }
+            return "";
+        }
+
+
+        /**
+         *  Helper function, that extracts the first substring that occurs between two characters in a given string.
+         *  
+         *  @param str The input string to search.
+         *  @param start The starting character to search for.
+         *  @param end The ending character to search for.
+         *
+         *  @example: glsl_include::extract_first_between("extract_between_underscores", '_', '_'); // will return "between"
+         * 
+         *  @return The extracted string (excluding the symbols themselves), or an empty string if no match is found.
+         */
+        std::string extract_first_between(const std::string& input, char start_symbol, char end_symbol)
+        {
+            size_t start_index = input.find(start_symbol);
+            size_t end_index = input.find(end_symbol, start_index + 1);
+            std::string extracted_string = "";
+            if (start_index != std::string::npos && end_index != std::string::npos)
+            {
+                extracted_string = input.substr(start_index + 1, end_index - start_index - 1);
+            }
+            else
+            {
+                std::cout << "ERROR [ glsl_include::extract_first_between ]: Start '" << start_symbol << "' or end symbol '" << end_symbol << "' not found" << std::endl;
+            }
+            return extracted_string;
+        }
+    };
+}
+#endif
